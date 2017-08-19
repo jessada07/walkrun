ee3c85d68900115de864ee82189fe19e764a5dbc
jessada07 <jessada_drummer@hotmail.com>
2017-08-19 19:07:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<html lang="en">
+  <head>
+    <title>KMUTNB Running</title>
+    <!-- META -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
+    <link rel="icon" href="data:;base64,=">
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/bootstrap.css" />
+    <style>
+        body {
+            padding-top: 50px;
+            padding-bottom: 20px;
+        }
+    </style>
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/bootstrap-theme.css" />
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/main.css" />
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/menu.css" />
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/bootstrap-datepicker.min.css" />
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/css/dropzone.css" />
+    <link rel="stylesheet" href="http://alumni.eng.kmutnb.ac.th/walk_run/bower_components/bootstrap-toggle/css/bootstrap-toggle.min.css" />
+
+    <!-- Javascript -->
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery-1.11.2.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/bootstrap-datepicker.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/bootstrap.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/dropzone.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery.inputmask.bundle.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery.bootstrap-autohidingnavbar.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/bower_components/bootstrap-toggle/js/bootstrap-toggle.min.js" ></script>
+
+
+  </head>
+  <?php
+  // echo ($_GET['user_app_id']);
+  ?>
+  <body style="background-color: white;">
+    <nav class="navbar navbar-kmutnb navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="">KMUTNB Walk - Run 2017</a>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <center>
+         <div class="row" style="margin-top:10%;">
+           <div class="col-xs-6 col-xs-offset-3">
+             <img src="assets/img/goals.png" alt="Rounded Image" class="img-rounded img-responsive">
+           </div>
+         </div>
+         <div class="row" style="margin-top:5%;">
+           <div class="col-xs-12 col-xs-offset-0">
+             <h2 class="text-danger">ขออภัย!!!</h2>
+             <h3 class="text-danger">ผลการแข่งขันยังไม่เปิดให้บริการ</h3>
+           </div>
+         </div>
+      </center>
+         <!-- <div class="row">
+           <div class="col-xs-8 col-xs-offset-2">
+             <h3>ผลการแข่งขัน</h3>
+           </div>
+         </div>
+         </center>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Name :
+           </div>
+           <div class="col-xs-6 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Bib No :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Gender :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Type :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Time :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Overall Rank :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-6 col-xs-offset-0" style="text-align: right;">
+             Gender Overall Rank :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div>
+         <div class="row">
+           <div class="col-xs-5 col-xs-offset-1" style="text-align: right;">
+             Age Group Rank :
+           </div>
+           <div class="col-xs-4 col-xs-offset-0">
+             xxxxxxx
+           </div>
+         </div> -->
+     </div>
+  </body>
+  <!--   Core JS Files   -->
+  <!-- <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery-1.11.2.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/bootstrap-datepicker.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/bootstrap.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/dropzone.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery.inputmask.bundle.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/js/vendor/jquery.bootstrap-autohidingnavbar.min.js" ></script>
+    <script  src="http://alumni.eng.kmutnb.ac.th/walk_run/bower_components/bootstrap-toggle/js/bootstrap-toggle.min.js" ></script> -->
+</html>
